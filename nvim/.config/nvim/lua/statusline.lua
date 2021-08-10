cf1297e9b594fae0652d75f3862f29907a8d01b3
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2021-08-10 23:43:47 +0300

    feat(nvim statusline)

--- nvim/.config/nvim/lua/statusline.lua
+++ nvim/.config/nvim/lua/statusline.lua
@@ -1,27 +1,61 @@
+local api, lsp, cmd, opt = vim.api, vim.lsp, vim.cmd, vim.opt
+
 local function FugitiveInfo()
-	local branch = vim.fn["fugitive#head"]()
-	if branch == "" or not branch then
-		return "('git init')"
-	else
+	local active_buffer_number = tostring(api.nvim_get_var("actual_curbuf"))
+	local buffer_number = tostring(api.nvim_get_current_buf())
+	local branch = vim.fn.FugitiveHead()
+
+	if branch ~= "" or branch and active_buffer_number == buffer_number then
 		return "git(" .. branch .. ")"
+	else
+		return "not a git"
 	end
 end
 
 local function LspInfo()
-	local errors = vim.lsp.diagnostic.get_count(0, "Error")
-	local warnings = vim.lsp.diagnostic.get_count(0, "Warning")
-	local hints_and_info = vim.lsp.diagnostic.get_count(0, "Hint") + vim.lsp.diagnostic.get_count(0, "Information")
+	local active_buffer_number = tostring(api.nvim_get_var("actual_curbuf"))
+	local buffer_number = tostring(api.nvim_get_current_buf())
+	--local active_buffer_number = tostring(api.nvim_get_var("statusline_winid"))
+	--local buffer_number = tostring(api.nvim_win_get_number(api.nvim_get_current_win()))
 
-	return "%#LspDiagnosticsDefaultError#" .. errors ..
-				 "%*, %#LspDiagnosticsDefaultWarning#" .. warnings ..
-				 "%*, %#LspDiagnosticsDefaultHint#" .. hints_and_info .. "%*"
+	local errors = lsp.diagnostic.get_count(0, "Error")
+	local warnings = lsp.diagnostic.get_count(0, "Warning")
+	local hints_and_info = lsp.diagnostic.get_count(0, "Hint") + lsp.diagnostic.get_count(0, "Information")
+
+	local error_group, warnings_group, hints_and_info_group
+
+--StatusLine   cterm=reverse ctermfg=239 ctermbg=223 gui=reverse guifg=#504945 guibg=#ebdbb2
+	if errors > 0 and active_buffer_number == buffer_number then
+		cmd("hi StrErr ctermfg=167 guifg=#fb4934 ctermbg=239 guibg=#504945")
+		error_group = "%#StrErr#"
+	else
+		error_group = ""
+	end
+	if warnings > 0 and active_buffer_number == buffer_number then
+		cmd("hi StrWarning ctermfg=214 guifg=#fabd2f ctermbg=239 guibg=#504945")
+		warnings_group = "%#StrWarning#"
+	else
+		warnings_group = ""
+	end
+	if hints_and_info > 0 and active_buffer_number == buffer_number then
+		cmd("hi StrHint ctermfg=108 guifg=#8ec07c ctermbg=239 guibg=#504945")
+		hints_and_info_group = "%#StrHint#"
+	else
+		hints_and_info_group = ""
+	end
+
+	return error_group .. errors .. "%*, " ..
+				 warnings_group .. warnings .. "%*, " ..
+				 hints_and_info_group .. hints_and_info .. "%*"
 end
 
 function StatusString()
 	local persent_sign = "%%"
-	local status_string = " %%-10.(:b %%n%%m%%) %%<%%f : %s    [%s] %%= %%L (%%p%s) | %%c "
-	return string.format(status_string, FugitiveInfo(), LspInfo(), persent_sign)
+	local left_side = string.format(" :b %%n%%m | %s%%=", LspInfo())
+	local center = string.format("%%f | %s%%=", FugitiveInfo())
+	local right_side = string.format("%%L (%%p%s) | %%c ", persent_sign)
+	return left_side .. center .. right_side
 end
 
-vim.o.statusline = "%!v:lua.StatusString()"
+opt.statusline = "%{%v:lua.StatusString()%}"
 
