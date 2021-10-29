7ae8c3815cab8b987e2c442393e3ff516de1279c
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2021-10-29 16:02:22 +0300

    fix(nvim) vim-polyglot -> ftplugin

---
+++ nvim/.config/nvim/ftplugin/gdscript.lua
@@ -0,0 +1,11 @@
+local opt = vim.opt
+
+opt.autoindent = true
+opt.copyindent = true
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.smartindent = true
+opt.smarttab = true
+opt.softtabstop = 4
+opt.tabstop = 4
+
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -2,31 +2,30 @@ vim.cmd([[packadd packer.nvim]])
 
 return require("packer").startup(function()
 
-  use "wbthomason/packer.nvim"
-  use "gruvbox-community/gruvbox"
-  use {
-    "rose-pine/neovim", as = "rose-pine"
-  }
-  use "kyazdani42/nvim-tree.lua"
-  use "neovim/nvim-lspconfig"
-  use {
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-  }
-  use "hrsh7th/cmp-nvim-lsp"
-  use "hrsh7th/cmp-buffer"
-  use "hrsh7th/nvim-cmp"
-  use "sheerun/vim-polyglot"
-  use "tpope/vim-fugitive"
-  use "OmniSharp/omnisharp-vim"
-  use {
-    "dense-analysis/ale",
-    ft = { "cs" },
-  }
-  use {
-    "akinsho/flutter-tools.nvim",
-    requires = "nvim-lua/plenary.nvim"
-  }
+	use "wbthomason/packer.nvim"
+	use "gruvbox-community/gruvbox"
+	use {
+		"rose-pine/neovim", as = "rose-pine"
+	}
+	use "kyazdani42/nvim-tree.lua"
+	use "neovim/nvim-lspconfig"
+	use {
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	}
+	use "hrsh7th/cmp-nvim-lsp"
+	use "hrsh7th/cmp-buffer"
+	use "hrsh7th/nvim-cmp"
+	use "tpope/vim-fugitive"
+	use "OmniSharp/omnisharp-vim"
+	use {
+		"dense-analysis/ale",
+		ft = { "cs" },
+	}
+	use {
+		"akinsho/flutter-tools.nvim",
+		requires = "nvim-lua/plenary.nvim"
+	}
 
 end)
 
