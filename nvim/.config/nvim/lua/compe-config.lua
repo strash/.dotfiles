87112d7f887032cbb669f9a470576660b12b60af
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2021-10-29 18:08:29 +0300

    fix(nvim) nvim-cmp mapping

--- nvim/.config/nvim/lua/compe-config.lua
+++ nvim/.config/nvim/lua/compe-config.lua
@@ -18,7 +18,7 @@ cmp.setup({
 			i = cmp.mapping.abort(),
 			c = cmp.mapping.close(),
 		}),
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		--["<CR>"] = cmp.mapping.confirm({ select = true }),
 	},
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
