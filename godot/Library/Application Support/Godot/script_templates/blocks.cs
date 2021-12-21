1cfa2be424a6afd26c58dd87c320771dc7f7265b
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2021-12-21 22:22:41 +0300

    fixed godot. added blocks.cs template

---
+++ godot/Library/Application Support/Godot/script_templates/blocks.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using Godot;
+
+public class %CLASS% : %BASE%
+{
+    // PROPERTIES -------------------------
+
+
+    // BUILTINS ------------------------
+
+
+    // PRIVATE METHODS -------------------------
+
+
+    // PUBLIC METHODS -------------------------
+
+
+    // SIGNALS -------------------------
+}
