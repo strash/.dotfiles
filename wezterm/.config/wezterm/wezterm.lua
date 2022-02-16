af8eca99f542ce5a5b7ac90776d2c87e69dc8c5f
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2022-02-16 22:47:45 +0300

    done with anime

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,13 +1,5 @@
 local wezterm = require("wezterm");
 
-wezterm.on("change_wallpapper", function(window, _)
-	math.randomseed(os.time())
-	local anime = math.random(22)
-	local overrides = window:get_config_overrides() or {}
-	overrides.window_background_image = "./wallpappers/" .. anime .. ".jpg",
-	window:set_config_overrides(overrides)
-end)
-
 return {
 	dpi = 144.0,
 	font = wezterm.font("SF Mono", { weight = "Regular", italic = false }),
@@ -16,7 +8,7 @@ return {
 	hide_tab_bar_if_only_one_tab = true,
 	color_scheme = "GitHub Dark",
 	use_fancy_tab_bar = false,
-	window_background_image = "./wallpappers/5.jpg",
+	--window_background_image = "./wallpappers/5.jpg",
 	window_background_image_hsb = {
 		brightness = 0.06,
 	},
@@ -50,7 +42,5 @@ return {
 	},
 	keys = {
 		{ key = "h", mods = "ALT", action = "ShowLauncher" },
-		{ key = "t", mods = "CMD|SHIFT", action = wezterm.action{EmitEvent = "change_wallpapper"} },
-		{ key = "r", mods="CMD|SHIFT", action="ReloadConfiguration" },
 	},
 }
