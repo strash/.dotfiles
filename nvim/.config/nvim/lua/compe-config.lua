a1b8eb7cecd36bda4c882f4bc1e9e02340055cea
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2022-04-14 18:26:18 +0300

    Updated nvim compe-config, lsp-config

--- nvim/.config/nvim/lua/compe-config.lua
+++ nvim/.config/nvim/lua/compe-config.lua
@@ -1,10 +1,14 @@
 local cmp = require("cmp")
 
 cmp.setup({
-	 mapping = cmp.mapping.preset.insert({
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
+		["<TAB>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.abort(),
 		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 	}),
@@ -20,14 +24,5 @@ cmp.setup.cmdline("/", {
 	sources = {
 		{ name = "buffer" }
 	}
-})
-
-cmp.setup.cmdline(":", {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({
-		{ name = "path" }
-	}, {
-		{ name = "cmdline" }
-	})
 })
 
--- nvim/.config/nvim/lua/lsp-config.lua
+++ nvim/.config/nvim/lua/lsp-config.lua
@@ -1,14 +1,15 @@
 local nvim_lsp = require("lspconfig")
 local map = require("map")
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local servers = {
 	"clangd",
 	"cssls",
 	--"csharp_ls",
-	"gdscript",
+	--"gdscript",
 	"html",
 	"jsonls",
 	"prismals",
@@ -30,8 +31,18 @@ for _, lsp in ipairs(servers) do
 	}
 end
 
+-- godot
+nvim_lsp.gdscript.setup({
+	cmd = { "nc", "localhost", "6008" },
+	filetypes = { "gd", "gdscript", "gdscript3", "res", "tres", "shader", "godot" },
+	root_dir = nvim_lsp.util.root_pattern("progect.godot", ".git"),
+	on_attach = map.set_lsp_map,
+	capabilities = capabilities,
+	flags = flags,
+})
+
 -- bashls
-nvim_lsp.bashls.setup {
+nvim_lsp.bashls.setup({
 	cmd = { "bash-language-server", "start" },
 	cmd_env = {
 		GLOB_PATTERN = "*@(.zsh|.sh|.inc|.bash|.command)",
@@ -42,10 +53,10 @@ nvim_lsp.bashls.setup {
 	on_attach = map.set_lsp_map,
 	capabilities = capabilities,
 	flags = flags,
-}
+})
 
 -- omnisharp
-nvim_lsp.omnisharp.setup{
+nvim_lsp.omnisharp.setup({
 	cmd = {
 		"/Users/strash/.cache/omnisharp-vim/omnisharp-roslyn/run",
 		"--languageserver",
@@ -55,14 +66,14 @@ nvim_lsp.omnisharp.setup{
 	on_attach = map.set_lsp_map,
 	capabilities = capabilities,
 	flags = flags,
-}
+})
 
 -- sumneko lua
 local runtime_path = vim.split(package.path, ";")
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
-nvim_lsp.sumneko_lua.setup {
+nvim_lsp.sumneko_lua.setup({
 	on_attach = map.set_lsp_map,
 	capabilities = capabilities,
 	flags = flags,
@@ -83,5 +94,5 @@ nvim_lsp.sumneko_lua.setup {
 			},
 		},
 	},
-}
+})
 
