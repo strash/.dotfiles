a5966ddb37aee6c86ce37d1b083c87d05d8eb396
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2022-04-14 19:39:06 +0300

    Updated nvim compe-config

--- nvim/.config/nvim/lua/compe-config.lua
+++ nvim/.config/nvim/lua/compe-config.lua
@@ -10,7 +10,7 @@ cmp.setup({
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<TAB>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 	}),
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
