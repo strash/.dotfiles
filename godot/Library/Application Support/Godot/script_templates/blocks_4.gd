1a21962b6f0e8f176a7db6450dccba2b56152e0a
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2022-12-10 00:58:36 +0300

    update: nvim and godot things

---
+++ godot/Library/Application Support/Godot/script_templates/blocks_4.gd
@@ -0,0 +1,18 @@
+# meta-name: Blocks
+# meta-description: Custom template for Godot v4
+# meta-default: true
+extends _BASE_
+
+
+# BUILTIN -------------------------
+
+
+# PRIVATE -------------------------
+
+
+# PUBLIC -------------------------
+
+
+# SIGNALS -------------------------
+
+
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -24,4 +24,5 @@ require("plugins.treeshitter-context")
 require("buffer_manager").setup({
 	width = 120,
 	height = 12,
+	focus_alternate_buffer = true,
 })
