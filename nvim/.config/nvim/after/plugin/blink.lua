29d4a7cb00527fd4795b01b550d81aa0400b6552
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2024-10-31 08:27:32 +0300

    update(nvim): blink, colo

--- nvim/.config/nvim/after/plugin/blink.lua
+++ nvim/.config/nvim/after/plugin/blink.lua
@@ -3,13 +3,13 @@ local p = require("plugin_loader").load("blink.cmp")
 if p ~= nil then
 	require("blink.cmp").setup({
 		keymap = {
-			["<CR>"] = { "accept", "select_and_accept" },
-			["<C-f>"] = { "show", "show_documentation", "hide_documentation" },
-			["<C-e>"] = { "hide" },
-			["<C-p>"] = { "select_prev" },
-			["<C-n>"] = { "select_next" },
-			["<C-k>"] = { "scroll_documentation_up" },
-			["<C-j>"] = { "scroll_documentation_down" },
+			["<CR>"] = { "accept", "select_and_accept", "fallback" },
+			["<C-f>"] = { "show", "show_documentation", "hide_documentation", "fallback" },
+			["<C-e>"] = { "hide", "fallback" },
+			["<C-p>"] = { "select_prev", "fallback" },
+			["<C-n>"] = { "select_next", "fallback" },
+			["<C-k>"] = { "scroll_documentation_up", "fallback" },
+			["<C-j>"] = { "scroll_documentation_down", "fallback" },
 			["<S-Tab>"] = { "snippet_backward", "fallback" },
 			["<Tab>"] = {
 				function(cmp)
