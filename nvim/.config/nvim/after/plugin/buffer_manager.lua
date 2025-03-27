60f915d94cc647c0ff2ff908f9f7ca454757d567
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2025-03-27 16:20:29 +0300

    update(nvim): buffer manager

--- nvim/.config/nvim/after/plugin/buffer_manager.lua
+++ nvim/.config/nvim/after/plugin/buffer_manager.lua
@@ -2,8 +2,8 @@ local p = require("plugin_loader").load("buffer_manager")
 
 if p ~= nil then
 	require("buffer_manager").setup({
-		width = 100,
-		height = 30,
+		width = 90,
+		height = 26,
 		focus_alternate_buffer = true,
 		short_term_names = true,
 		show_indicators = nil,
