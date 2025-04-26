96ce286dfc2aa4e0676b902f6d46d3ca0238f9c0
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2025-04-26 16:46:25 +0300

    update(fish, helix)

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,7 +3,7 @@ if status is-interactive
 end
 abbr -a -- l 'ls -lahG'
 abbr -a -- go_clean 'go clean -x -cache -modcache'
-abbr -a -- boocka 'bun update -g && npm update -g && arch -arm64 brew update && arch -arm64 brew outdated && arch -arm64 brew upgrade --greedy && brew cleanup -s && brew autoremove'
+abbr -a -- boocka 'arch -arm64 brew update && arch -arm64 brew outdated && arch -arm64 brew upgrade --greedy && brew cleanup -s && brew autoremove && bun update -g'
 abbr -a -- firebase_update 'curl -sL https://firebase.tools | upgrade=true bash'
 abbr -a -- bdump 'brew bundle dump --describe -f'
 abbr -a -- bdinstall 'arch -arm64 brew bundle --no-lock'
