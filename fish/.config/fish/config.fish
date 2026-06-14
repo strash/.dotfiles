6e970ee81794be73fd62181963e61ee040d01df9
Dmitry Poyarkov <14233263+Strash@users.noreply.github.com>
2026-06-14 19:18:37 +0300

    update: fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 abbr -a -- go_clean 'go clean -x -cache -modcache'
-abbr -a -- boocka 'brew update && brew outdated && brew upgrade --greedy && brew cleanup -s && brew autoremove && bun update -g'
+abbr -a -- boocka 'brew update && brew outdated && brew upgrade --greedy --yes && brew cleanup --scrub && brew autoremove && bun update -g'
 abbr -a -- firebase_update 'curl -sL https://firebase.tools | upgrade=true bash'
 abbr -a -- bdump 'brew bundle dump --describe -f'
 abbr -a -- bdinstall 'brew bundle --no-lock'
